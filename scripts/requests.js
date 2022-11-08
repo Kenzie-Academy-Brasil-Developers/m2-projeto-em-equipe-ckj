b98108c928a7ae1c1a231a61a00b0d152dbd3b56
cadu-pavin <cadupavin@hotmail.com>
2022-11-08 18:14:10 -0300

    request new user

--- scripts/requests.js
+++ scripts/requests.js
@@ -20,4 +20,24 @@ async function requestAllPets (){
     }
 }
 
+async function requestSingUp (user){
+    try{
+        const request = await fetch(baseUrl + "users", {
+            method: "POST",
+            Headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(user)
+        })
+
+        if(request.ok){
+            const response = await request.json()
+
+            return response
+        }
+    }catch(err){
+            console.log(err)
+    }
+}
+
 export{requestAllPets,}
